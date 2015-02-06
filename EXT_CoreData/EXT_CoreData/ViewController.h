d8c8b8c779cf10f03b81931aee469edad7de756f
xiaoboJoin <xiaobo_duan@126.com>
2015-02-06 11:20:16 +0800

    CoreData
    initial

---
+++ EXT_CoreData/EXT_CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EXT_CoreData
+//
+//  Created by iXiaobo on 15-2-6.
+//  Copyright (c) 2015年 iXiaobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EXT_CoreData/EXT_CoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EXT_CoreData
+//
+//  Created by iXiaobo on 15-2-6.
+//  Copyright (c) 2015年 iXiaobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
